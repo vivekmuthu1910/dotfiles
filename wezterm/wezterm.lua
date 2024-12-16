a60e7e29e76ac6225d854b2d74ea2d18f9385b98
Vivekanandan Muthaiyan <vivekmuthu1910@gmail.com>
2024-12-16 07:19:50 +0530

    Initial Wezterm Config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+config.front_end = 'WebGpu'
+config.webgpu_power_preference = 'HighPerformance'
+config.enable_wayland = false
+
+-- and finally, return the configuration to wezterm
+return config
